4159fe2ba04d4dcc6d7f257a3243175adc0f1be4
fbtjrrb95 <58498606+fbtjrrb95@users.noreply.github.com>
2021-04-21 20:39:59 +0900

    redisTemplate autowired가 아닌 mockito를 이용하여 test code 작성하기

--- src/test/java/me/screw/demobootredis/service/RedisServiceTest.java
+++ src/test/java/me/screw/demobootredis/service/RedisServiceTest.java
@@ -1,45 +1,80 @@
 package me.screw.demobootredis.service;
 
+import me.screw.demobootredis.repository.UsersRedisRepository;
+import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.mockito.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class RedisServiceTest {
 
-    @Autowired
+    @InjectMocks
     private RedisService redisService;
 
-    @Autowired
+
+    @Mock
     private RedisTemplate<String, String> redisTemplate;
 
+    @Mock
+    private ListOperations<String, String> mockTokenList;
+
+//    @Mock
+//    private UsersRedisRepository usersRedisRepository;
+
+
     @BeforeEach
-    public void initialize() {
-        while(redisTemplate.opsForList().size("token") >0) {
-            redisTemplate.opsForList().rightPop("token");
-        }
-    }
-    @Test
-    void initializeToken() {
-        redisService.initializeToken(10);
-        assertEquals(redisTemplate.opsForList().size("token"), 10l);
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        Mockito.when(redisTemplate.opsForList()).thenReturn(mockTokenList);
+
+//        Mockito.doNothing().when(mockTokenList).set("token", 1L, "coupon_uuid");
     }
 
     @Test
-    void getToken() {
-        redisTemplate.opsForList().rightPush("token","coupon_uuid");
-        assertEquals(redisService.getToken(),"coupon_uuid");
+    public void initializeToken() {
+        Mockito.when(redisTemplate.opsForList().size("token")).thenReturn(10L);
+        assertEquals(10L, redisService.initializeToken(10));
     }
 
-    @Test
-    void getNullToken() {
-        assertEquals(redisService.getToken(), null);
-    }
+//    @Test
+//    public void getToken() {
+//        redisTemplate.opsForList().rightPush("token","coupon_uuid");
+//        assertEquals(redisService.getToken(),"coupon_uuid");
+//    }
+//
+//    @Test
+//    public void getNullToken() {
+//        assertEquals(redisService.getToken(), null);
+//    }
+
+//    @Test
+//    public void getUsers(){
+//
+//    }
+//
+//    @Test
+//    public void setUsers() {
+//
+//    }
+//
+//    @Test
+//    public void isExist() {
+//
+//    }
+
 }
