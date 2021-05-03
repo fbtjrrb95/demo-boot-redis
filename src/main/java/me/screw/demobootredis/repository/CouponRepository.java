c6b94d07d971356a93aaa1c1104d00f5085d152c
fbtjrrb95 <58498606+fbtjrrb95@users.noreply.github.com>
2021-05-03 23:54:44 +0900

    오타 수정

--- src/main/java/me/screw/demobootredis/repository/CouponRepository.java
+++ src/main/java/me/screw/demobootredis/repository/CouponRepository.java
@@ -4,12 +4,10 @@ import me.screw.demobootredis.domain.Coupons;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-@Transactional
 public interface CouponRepository extends JpaRepository<Coupons, Long> {
     Optional<Coupons> findById(Long id);
 
